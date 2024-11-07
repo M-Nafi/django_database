179c365abaaf0cb0183c5f921175171d759a9862
M-Nafi <nafi-m@gmx.de>
2024-11-07 13:49:27 +0100

    removed something in settings.py

--- django_database/settings.py
+++ django_database/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qb64l1_9!gqwk625$lj7e2b25g0e_qmusr&bqcn+&=8rwriz8u'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
